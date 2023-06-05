ad162cf0defcfe86c8eb48260d79a87cacd6801f
simrett <leesim242@gmail.com>
2023-06-05 11:44:08 -0700

    slicing to extract sentences

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-s = str[38:66]
+s = str[39:66]
 print(s + " with python")

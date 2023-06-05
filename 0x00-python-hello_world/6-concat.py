43d3e3229bd72e16153618461e5f6f6cd8ecbc64
simrett <leesim242@gmail.com>
2023-06-05 11:29:37 -0700

    playing with strings

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-concat_str = str1 + " " + str2
+str1 = str1 + str2
 print(f"Welcome to {str1}!")
+str = str1 + " " + str2
+print(f"Welcome to {str}!")

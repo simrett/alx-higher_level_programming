7d6cc1858e1886eb3680fdc5bb95d27b2c49c9ba
simrett <leesim242@gmail.com>
2023-06-07 10:14:00 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for c in range(122, 96, -1):
     if not(c % 2 == 0):
         print("{}".format(chr(c).upper()), end="")
     else:
-        print("{}".format(chr(c)), end="")        
+        print("{}".format(chr(c)), end="")

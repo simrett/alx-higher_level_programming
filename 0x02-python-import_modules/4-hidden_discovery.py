f215b0e6cb73dc68785ce93943ca5f78f5908958
simrett <leesim242@gmail.com>
2023-06-08 09:32:25 -0700

    4-hidden_discovery.py correct output check

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for name in sorted(dir(hidden_4)):
-        if not name.startwith("_"):
+        if not name.startswith("__"):
             print("{}".format(name))

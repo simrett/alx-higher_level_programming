c8e9bcc78026dca745faa393636c5e81af2eed1c
simrett <leesim242@gmail.com>
2023-06-08 09:23:51 -0700

    3-infinite_add.py correct variable

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    narg = 0
+    argn = 0
     for i in range(1, len(sys.agrv)):
-        narg += int(sys.argv[i])
-    print("{}".format(narg))
+        argn += int(sys.argv[i])
+    print("{}".format(argn))

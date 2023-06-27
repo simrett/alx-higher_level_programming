921d2967d87acbf02b51d655aecd9fb97deac3cb
simrett <leesim242@gmail.com>
2023-06-27 09:01:00 -0700

    private attribute in class py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,11 +2,13 @@
 
 class square:
     """
-    Class represents a square.
+    Class square that has attributes:
+        size
 
-    Attributes:
-    size
     """
 
     def __init__(self, size):
+        """
+        the initialization function for the square class
+        """
         self.__size = size

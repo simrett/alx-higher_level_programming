94fce1c44de3c8ffba7c59e29d24b50f15d80a4a
simrett <leesim242@gmail.com>
2023-07-04 16:59:43 -0700

    blanklines in testing

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a text-indentation function."""
+
+
+def text_indentation(text):
+    """
+    prints a string of text with 2 new lines after '.', '?', and ':'
+    unit tests located in tests/5-text_indentation.txt
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    string = ""
+    specials = ['.', '?', ':']
+    for ch in text:
+        string += ch
+        if ch in specials:
+            string += "\n\n"
+    print(string, end='')

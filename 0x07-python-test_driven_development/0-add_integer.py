e44a895e013b2ec0d3944bb6b51f83dc9df38a7a
simrett <leesim242@gmail.com>
2023-07-04 16:47:56 -0700

    unit tests for test driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""an integer addition function."""
+
+
+def add_integer(a, b=98):
+    """
+    adds an integer
+    unit tests located in tests/0-add_integer.txt
+    checks for type errors
+    """
+    if not isinstance(a, int):
+        if isinstance(a, float):
+            a = int(a)
+        else:
+            raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        if isinstance(b, float):
+            b = int(b)
+        else:
+            raise TypeError("b must be an integer")
+    return a + b

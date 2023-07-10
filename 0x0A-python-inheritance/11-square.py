feeba2dd431072a37ce43d88d522fec6b36d50ca
simrett <leesim242@gmail.com>
2023-07-10 12:06:18 -0700

    11 square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
 """ Import the Basegeometry module """
 
-
 Rectangle = __import__('9-rectangle').Rectangle
+
+""" A derived class that inherits from Basegeometry class """
 
 
 class Square(Rectangle):
-    """square shape class, super class is BaseGeometry, then Rectangle
-    """
+
+    """ inistantiates with size """
     def __init__(self, size):
-        """instantiation method for class
-        """
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
-        """overide magic str method for class
-        """
-        string = "[Square] " + str(self.__size) + '/'
-        string += str(self.__size)
-        return string
+        return "[Sqaure] {}/{}".format(self.__size, self.__size)

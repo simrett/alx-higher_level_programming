bd9ec833461e3baa3bc788f365817785ff7c538c
simrett <leesim242@gmail.com>
2023-07-11 11:34:42 -0700

    append after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a text file insertion function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """appends after search string instances in file
+    """
+    with open(filename, 'r', encoding='utf-8') as myFile:
+        lines = myFile.readlines()
+
+    for i, line in enumerate(lines):
+        if search_string in line:
+            lines.insert(i + 1, new_string)
+    with open(filename, 'w', encoding='utf-8') as myFile:
+        content = "".join(lines)
+        myFile.write(content)

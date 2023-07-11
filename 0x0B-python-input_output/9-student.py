54552513cae4a7aa88bf4a95ce1569b8cdac7b17
simrett <leesim242@gmail.com>
2023-07-11 11:31:53 -0700

    modified

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
+"""Defines a class Student."""
 
 
 class Student:
-    """Represent a student.
-    """
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Gets dictionary representation of Student in json.
-        """
-        return {key: value for (key, value) in self.__dict__.items()
-                if key in list(self.__dict__.keys())}
+        """Get a dictionary representation of the Student."""
+        return self.__dict__

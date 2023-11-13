62458aea93f08ddb579e2450b5be341a0b0f3fc8
simrett <leesim242@gmail.com>
2023-11-13 12:56:04 -0800

    mod

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
+// Ensure Node.js version compatibility (version 14.x)
+// This code is semistandard compliant (version 16.x.x), following Standard + semicolons on top
+
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
+
+// Ensure there is a newline at the end of the file
 

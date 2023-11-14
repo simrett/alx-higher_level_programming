0c8414473516dfb9f7d17d569c70ac5c522f9fc2
simrett <leesim242@gmail.com>
2023-11-14 13:52:52 -0800

    prints the rectangle using the character X

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return this;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    if (!this.width || !this.height) {
+      console.log('Empty Rectangle');
+      return;
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Rectangle;

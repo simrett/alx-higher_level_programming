8def7647280761f69a8a33c380b4de20c2a1f430
simrett <leesim242@gmail.com>
2023-12-05 07:26:11 -0800

    modified

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""
-Python script that takes your Github credentials (username and password)
-and uses the Github API to display your id
-"""
-import requests
+""" get user commit and name from github """
+
+import sys
+from requests
 from requests.auth import HTTPBasicAuth
-from sys import argv
 
 if __name__ == '__main__':
-    url = 'https://api.github.com/users/{}'.format(argv[1])
-    r = requests.get(url,
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+    url = 'https://api.github.com/users'
+    user = sys.argv[1]
+    passwrd = sys.argv[2]
+    res = requests.get(url, auth=HTTPBasicAuth(user, passwrd))
+    print(res.json().get('id'))

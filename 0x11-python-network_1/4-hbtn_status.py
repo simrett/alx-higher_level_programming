9f691e01cb2e52b61f9b4854240c89477993490a
simrett <leesim242@gmail.com>
2023-12-05 07:12:15 -0800

    status py error fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """
-Script that fetches https://intranet.hbtn.io/status
+Send request and display the X-Request-Id value
 """
 import requests
 
 if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
+    res = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type:", type(res.text))
+    print("\t- content:", res.text)

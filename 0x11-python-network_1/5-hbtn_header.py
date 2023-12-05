e5d3a8933ebd5a50a82d246983572b5f2c6783dc
simrett <leesim242@gmail.com>
2023-12-05 07:20:20 -0800

    modified

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
-Script that takes in a URL and an email address,
-sends a POST request to the passed URL
-with the email as a parameter,
-and finally displays the body of the response
-"""
+""" send request and display the X-Request-Id inside the response """
+
+import sys
 import requests
-from sys import argv
 
 if __name__ == '__main__':
-    payload = {'email': argv[2]}
-    r = requests.post(argv[1], data=payload)
-    print(r.text)
+    res = requests.get(sys.argv[1])
+    print(res.headers.get('X-Request-Id'))
